30cfdd8cddc0eaaa5d52d93096f8b79774156fde
Michele_Manco <motlamik@gmail.com>
2019-11-25 18:09:30 +0100

    done?

---
+++ main.js
@@ -0,0 +1,8 @@
+$(".hamburger-menu").click(function(){
+  $(".hamburger-menu").addClass(".active")
+
+})
+$(".hamburger-menu").dbclick(function(){
+  $(".hamburger-menu").removeClass(".active")
+
+})
